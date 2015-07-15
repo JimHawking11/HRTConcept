30524da72171ac3dc6f796f7b8b9e2be4b317669
Mike Salkin <jimhawking11@gmail.com>
2015-07-15 01:45:55 -0500

    -Button UI -Prefab Fixes -Graphical Fix

---
+++ Assets/Scripts/TurnPlayer.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnPlayer : MonoBehaviour {
+
+	public GameObject playerObject;
+	public float speed = 70.0f;
+	private bool isLeft = false;
+	private bool isRight = false;
+	
+	public void Update()
+	{
+		if(isLeft && isRight)
+		{
+			return;
+		}
+		else if (isLeft)
+		{
+			print ("Rotating Left");
+			playerObject.transform.Rotate(-Vector3.up * speed * Time.deltaTime);
+		}
+		else if (isRight)
+		{
+			print ("Rotating Right");
+			playerObject.transform.Rotate(Vector3.up * speed * Time.deltaTime);
+		}
+	}
+	
+	public void LeftPointerDown()
+	{
+		isLeft = true;
+	}
+	
+	public void LeftPointerUp()
+	{
+		isLeft = false;
+	}
+	
+	public void RightPointerDown()
+	{
+		print ("Right Down");
+		isRight = true;
+	}
+	
+	public void RightPointerUp()
+	{
+		print ("Right Up");
+		isRight = false;
+	}
+	
+	
+}
